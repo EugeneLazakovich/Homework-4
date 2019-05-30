3fb19d2877795dd2d8759044549a205410512eaf
EugeneLazakovich <zlazakovich@gmail.com>
2019-05-30 23:13:33 +0300

    My homework4, Lazakovich

---
+++ Program (2).cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 11;
+            int[] sbyteAr = new int[n];
+            int[] shortAr = new int[n];
+            int[] intAr = new int[n];
+            Random rnd = new Random();
+            int sbyteCount = 0;
+            int shortCount = 0;
+            int intCount = 0;
+            int count = 0;
+            for(int i = 0; i < 2147483647; i++)
+            {
+                int rndNumber = rnd.Next(2147483647);
+                if (rndNumber < 128 && sbyteCount < (n - 1))
+                {
+                    sbyteAr[sbyteCount] = rndNumber;
+                    Console.WriteLine("sbyteAr["+sbyteCount+"]: "+ rndNumber);
+                    sbyteCount++;
+                }
+                else
+                {
+                    if(rndNumber<32768 && shortCount < (n - 1))
+                    {
+                        shortAr[shortCount] = rndNumber;
+                        Console.WriteLine("shortAr[" + shortCount + "]: " + rndNumber);
+                        shortCount++;
+                    }
+                    else
+                    {
+                        if (intCount < (n-1))
+                        {
+                            intAr[intCount] = rndNumber;
+                            Console.WriteLine("intAr[" + intCount + "]: " + rndNumber);
+                            intCount++;
+                        }
+                    }
+                }
+                if(sbyteCount== (n - 1) && shortCount== (n - 1) && intCount == (n - 1))
+                {
+                    break;
+                }                
+                count++;
+            }
+            Console.WriteLine("Counter: " + count);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 18, 6, -23, 64, 55 };
+            int temp;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
